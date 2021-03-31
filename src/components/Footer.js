15f98cef34e6cecc4e553fd8c0d6bc50f817421c
sjproctor <sjproctor950@gmail.com>
2021-03-30 19:31:03 -0700

    adds styling to header, footer, home, not found

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react'
+import { Nav, NavItem, NavLink } from 'reactstrap'
 
-class Footer extends Component {
-  render() {
-    return (
-      <>
-        Footer
-      </>
+class Footer extends Component{
+  render(){
+    return(
+      <footer>
+        <Nav>
+          <NavItem>
+            <span className="nav-link">&copy; 2021 Sarah & Mina</span>
+          </NavItem>
+          <NavItem>
+            <NavLink href="/">Home</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="/catindex">Meet the Cats</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="/catnew">Add a Cat</NavLink>
+          </NavItem>
+        </Nav>
+      </footer>
     )
   }
 }
